eec46d1d4b55e6581ce9732629102837fd12bb10
Jordan Goddard <jordangoddard6@gmail.com>
2025-04-07 15:38:21 -0600

    Got data from backend to pop up on admin page

--- frontend/src/services/movieService.ts
+++ frontend/src/services/movieService.ts
@@ -1,11 +1,13 @@
-import axios from 'axios';
-import { Movie, MovieFilters, MovieResponse } from '../types/movie';
+import axios from "axios";
+import { Movie, MovieFilters, MovieResponse } from "../types/movie";
 
-const API_BASE_URL = 'http://localhost:5000/api'; // This will be replaced with your actual API URL
+const API_BASE_URL = "https://localhost:5000/movie"; // This will be replaced with your actual API URL
 
 export const movieService = {
   async getMovies(filters: MovieFilters): Promise<MovieResponse> {
-    const response = await axios.get(`${API_BASE_URL}/movies`, { params: filters });
+    const response = await axios.get(`${API_BASE_URL}/adminmovies`, {
+      params: filters,
+    });
     return response.data;
   },
 
@@ -14,22 +16,24 @@ export const movieService = {
     return response.data;
   },
 
-  async createMovie(movie: Omit<Movie, 'id'>): Promise<Movie> {
+  async createMovie(movie: Omit<Movie, "id">): Promise<Movie> {
     const response = await axios.post(`${API_BASE_URL}/movies`, movie);
     return response.data;
   },
 
-  async updateMovie(id: number, movie: Partial<Movie>): Promise<Movie> {
+  async updateMovie(id: string, movie: Partial<Movie>): Promise<Movie> {
     const response = await axios.put(`${API_BASE_URL}/movies/${id}`, movie);
     return response.data;
   },
 
-  async deleteMovie(id: number): Promise<void> {
+  async deleteMovie(id: string): Promise<void> {
     await axios.delete(`${API_BASE_URL}/movies/${id}`);
   },
 
   async rateMovie(id: number, rating: number): Promise<Movie> {
-    const response = await axios.post(`${API_BASE_URL}/movies/${id}/rate`, { rating });
+    const response = await axios.post(`${API_BASE_URL}/movies/${id}/rate`, {
+      rating,
+    });
     return response.data;
-  }
-}; 
+  },
+};
