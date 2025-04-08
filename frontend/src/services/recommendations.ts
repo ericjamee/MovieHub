da63da3bb66685d4e1bade16ca1d22b8cc2b3457
Jordan Goddard <jordangoddard6@gmail.com>
2025-04-08 13:59:43 -0600

    The frontend can consume the API

--- frontend/src/services/recommendations.ts
+++ frontend/src/services/recommendations.ts
@@ -1,10 +1,12 @@
 import { RecommendationResponse } from "../types/recommendation";
 
+const API_BASE_URL = "https://localhost:5000/recommendations";
+
 export async function fetchRecommendations(
   movieTitle: string
 ): Promise<string[]> {
-  const encodedTitle = encodeURIComponent(movieTitle);
-  const response = await fetch(`/api/recommendations/${encodedTitle}`);
+  //   const encodedTitle = encodeURIComponent(movieTitle);
+  const response = await fetch(`${API_BASE_URL}/${movieTitle}`);
 
   if (!response.ok) {
     throw new Error(`No recommendations found for "${movieTitle}"`);
