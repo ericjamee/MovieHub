926a28f3bc1e9157b56a35eeb61e17abce7acc87
ericjamee <153117365+ericjamee@users.noreply.github.com>
2025-04-09 14:22:09 -0600

    Revert "James"

--- frontend/src/config.ts
+++
@@ -1,8 +0,0 @@
-export const config = {
-    apiBaseUrl: process.env.NODE_ENV === 'production'
-        ? 'https://cineniche-team-3-8-backend-eehrgvh4fhd7f8b9.eastus-01.azurewebsites.net'
-        : 'https://localhost:5000',
-    recommendationsBaseUrl: process.env.NODE_ENV === 'production'
-        ? 'https://cineniche-team-3-8-backend-eehrgvh4fhd7f8b9.eastus-01.azurewebsites.net/recommendations'
-        : 'https://localhost:5000/recommendations'
-}; 
