899af6d9fbd1e344aa76998c7359f5ee5a96cc56
emmajean720 <167582619+emmajean720@users.noreply.github.com>
2025-04-09 15:54:43 -0600

    more security updates

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-
     port: 3000,
+    headers: {
+      'Content-Security-Policy': `
+        script-src 'self' 'unsafe-inline' 'unsafe-eval' blob:;
+        style-src 'self' 'unsafe-inline';
+        font-src 'self' data:;
+        img-src * data: blob:;
+        connect-src *;
+        object-src 'none';
+        frame-ancestors 'none';
+        base-uri 'self';
+        form-action 'self';
+      `.replace(/\n/g, ''),
+    },
   },
 });
+;
+
