2a5c8f7e6108b9f4e48ef6b75fa7c560c29f010c
Jordan Goddard <jordangoddard6@gmail.com>
2025-04-10 00:55:10 -0600

    CSP change

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,24 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
     headers: {
-      'Content-Security-Policy': `
-        script-src 'self' 'unsafe-inline' 'unsafe-eval' blob:;
-        style-src 'self' 'unsafe-inline';
-        font-src 'self' data:;
-        img-src * data: blob:;
-        connect-src *;
-        object-src 'none';
-        frame-ancestors 'none';
-        base-uri 'self';
-        form-action 'self';
-      `.replace(/\n/g, ''),
+      "Content-Security-Policy": `
+      default-src 'self';
+      script-src 'self';
+      style-src 'self' 'unsafe-inline';
+      font-src 'self' data:;
+      img-src 'self' data:;
+      connect-src 'self' https://cineniche-team-3-8-backend-eehrgvh4fhd7f8b9.eastus-01.azurewebsites.net;
+      object-src 'none';
+      frame-ancestors 'none';
+      base-uri 'self';
+      form-action 'self';
+    `.replace(/\n/g, ""),
     },
   },
 });
-;
-
