7a313816f96c8ae6450d5b8bcc243e3b7b1983e6
Jordan Goddard <jordangoddard6@gmail.com>
2025-04-10 15:24:27 -0600

    Praying that this is the answer

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,18 +6,5 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-    headers: {
-      "Content-Security-Policy": `
-        script-src 'self' 'unsafe-inline' 'unsafe-eval' blob:;
-      style-src 'self' 'unsafe-inline';
-      font-src 'self' data:;
-        img-src * data: blob:;
-        connect-src *;
-      object-src 'none';
-      frame-ancestors 'none';
-      base-uri 'self';
-      form-action 'self';
-      `.replace(/\n/g, ""),
-    },
   },
 });
