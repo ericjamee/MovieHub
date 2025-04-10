c3e3bb0db50ef37960b4ae6d87ede6acbab32da3
Jordan Goddard <jordangoddard6@gmail.com>
2025-04-10 16:32:45 -0600

    Revert "New test"
    This reverts commit 5f56d2a38f5e9eba0ca4030be1d14942147556f4.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,25 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: "dist", // Ensure this matches the output directory in Azure
-  },
   server: {
     port: 3000,
     headers: {
       "Content-Security-Policy": `
-        default-src 'self';
-        script-src 'self';
-        style-src 'self' 'unsafe-inline';
-        font-src 'self' data:;
-        img-src 'self' data:;
-        connect-src 'self' https://cineniche-team-3-8-backend-eehrgvh4fhd7f8b9.eastus-01.azurewebsites.net;
-        object-src 'none';
-        frame-ancestors 'none';
-        base-uri 'self';
-        form-action 'self';
+        script-src 'self' 'unsafe-inline' 'unsafe-eval' blob:;
+      style-src 'self' 'unsafe-inline';
+      font-src 'self' data:;
+        img-src * data: blob:;
+        connect-src *;
+      object-src 'none';
+      frame-ancestors 'none';
+      base-uri 'self';
+      form-action 'self';
       `.replace(/\n/g, ""),
     },
   },
