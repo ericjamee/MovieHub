66aae13adc225df3d6e8fe3c4a82ee7735b326af
Jordan Goddard <jordangoddard6@gmail.com>
2025-04-10 11:22:39 -0600

    CSP policy change to fix deployed site

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,16 +8,17 @@ export default defineConfig({
     port: 3000,
     headers: {
       "Content-Security-Policy": `
-        script-src 'self' 'unsafe-inline' 'unsafe-eval' blob:;
-        style-src 'self' 'unsafe-inline';
-        font-src 'self' data:;
-        img-src * data: blob:;
-        connect-src *;
-        object-src 'none';
-        frame-ancestors 'none';
-        base-uri 'self';
-        form-action 'self';
-      `.replace(/\n/g, ""),
+  default-src 'self';
+  script-src 'self';
+  style-src 'self' 'unsafe-inline';
+  font-src 'self' data:;
+  img-src 'self' data:;
+  connect-src 'self' https://cineniche-team-3-8-backend-eehrgvh4fhd7f8b9.eastus-01.azurewebsites.net;
+  object-src 'none';
+  frame-ancestors 'none';
+  base-uri 'self';
+  form-action 'self';
+`.replace(/\n/g, ""),
     },
   },
 });
